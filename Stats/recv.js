3389d27dc36ea4ac0828abf5b376d807ba02f2ab
Stan Behrens <git@sbeh.de>
2018-11-21 21:39:06 +0100

    recv.js: Used to persist diagnostic data from BHKW with timestamp

---
+++ Stats/recv.js
@@ -0,0 +1,61 @@
+'use strict'
+
+process.on('multipleResolves', () => {
+    console.error('process.multipleResolves', arguments)
+    stop()
+})
+process.on('rejectionHandled', () => {
+    console.error('process.rejectionHandled', arguments)
+    stop()
+})
+process.on('uncaughtException', () => {
+    console.error('process.uncaughtException', arguments)
+    stop()
+})
+process.on('unhandledRejection', () => {
+    console.error('process.unhandledRejection', arguments)
+    stop()
+})
+
+var config = JSON.parse(require('fs').readFileSync('config.json'))
+
+var sock
+
+var start = () => {
+    sock = require('net').createConnection(config.port, config.host, () => {
+        console.error('Connected')
+
+        sock.setEncoding('utf8')
+        sock.setKeepAlive(true, 50 * 1000/* 50 seconds */)
+        sock.setTimeout(6 * 60 * 1000/* 6 minutes */)
+    })
+
+    sock.on('data', data => {
+        console.log(JSON.stringify({
+            time: Date.now(),
+            data: data
+        }))
+    })
+
+    sock.on('timeout', stop)
+    sock.on('end', stop)
+    sock.on('error', stop)
+    sock.on('close', stop)
+}
+
+var stop = () => {
+    try {
+        sock.removeAllListeners()
+        sock.destroy()
+    } catch(Exception) {
+
+    } finally {
+        sock = null
+    }
+
+    console.error('Disconnected')
+}
+
+process.on('beforeExit', () => {
+    setTimeout(start, 500)
+})
